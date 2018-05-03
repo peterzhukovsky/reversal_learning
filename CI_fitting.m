c547e50fe14accace8e86b00cf78665ba05786db
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2018-05-03 17:20:23 +0100

    Update CI_fitting.m

--- CI_fitting.m
+++ CI_fitting.m
@@ -5,7 +5,7 @@
         CIs=zeros(24,3)
         clear CI*
         
-        subject_list=[ 1     2     3     4     5    6     7     8     9    10    11    12  13 14   15    16    17  18  19 20    21    23    24];
+        subject_list=[ 1     2     3     4 ];
     for i=subject_list;
         
     neg_PDF=PDF_all{1,i}*(-1);
